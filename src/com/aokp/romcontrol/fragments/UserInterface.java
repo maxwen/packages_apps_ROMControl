e3e6a06bcdef9aaac48b36a40a3179f912b2c78b
Jonathan Steadman <xxwhitehawk@gmail.com>
2013-02-03 11:44:49 -0800

    RC: longpress to kill app option for hardware key
    P2: moved >>> General UI
        specified hardware key on strings
    
    Change-Id: If5bf69de187c241bf574c38bdd6cc3681cc26350
    Signed-off-by: Jonathan Steadman <xxwhitehawk@gmail.com>

--- src/com/aokp/romcontrol/fragments/UserInterface.java
+++ src/com/aokp/romcontrol/fragments/UserInterface.java
@@ -78,6 +78,7 @@ public class UserInterface extends AOKPPreferenceFragment implements OnPreferenc
     private static final String PREF_CUSTOM_CARRIER_LABEL = "custom_carrier_label";
     private static final String PREF_SHOW_OVERFLOW = "show_overflow";
     private static final String PREF_VIBRATE_NOTIF_EXPAND = "vibrate_notif_expand";
+    private static final String PREF_LONGPRESS_TO_KILL = "longpress_to_kill";
     private static final String PREF_RECENT_KILL_ALL = "recent_kill_all";
     private static final String PREF_RAM_USAGE_BAR = "ram_usage_bar";
     private static final String PREF_KILL_APP_LONGPRESS_BACK = "kill_app_longpress_back";
@@ -109,6 +110,7 @@ public class UserInterface extends AOKPPreferenceFragment implements OnPreferenc
     TextView error;
     CheckBoxPreference mShowActionOverflow;
     CheckBoxPreference mVibrateOnExpand;
+    CheckBoxPreference mLongPressToKill;
     CheckBoxPreference mRecentKillAll;
     CheckBoxPreference mRamBar;
     CheckBoxPreference mKillAppLongpressBack;
@@ -181,6 +183,15 @@ public class UserInterface extends AOKPPreferenceFragment implements OnPreferenc
         mVibrateOnExpand = (CheckBoxPreference) findPreference(PREF_VIBRATE_NOTIF_EXPAND);
         mVibrateOnExpand.setChecked(Settings.System.getBoolean(cr,
                 Settings.System.VIBRATE_NOTIF_EXPAND, true));
+
+        boolean hasHardwareButtons = mContext.getResources().getBoolean(
+                R.bool.has_hardware_buttons);
+        mLongPressToKill = (CheckBoxPreference)findPreference(PREF_LONGPRESS_TO_KILL);
+        mLongPressToKill.setChecked(Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.KILL_APP_LONGPRESS_BACK, 0) == 1);
+        if (!hasHardwareButtons) {
+            ((PreferenceGroup)findPreference("navbar")).removePreference(mLongPressToKill);
+        }
 
         mRecentKillAll = (CheckBoxPreference) findPreference(PREF_RECENT_KILL_ALL);
         mRecentKillAll.setChecked(Settings.System.getBoolean(cr,
@@ -445,6 +456,12 @@ public class UserInterface extends AOKPPreferenceFragment implements OnPreferenc
                     Settings.System.VIBRATE_NOTIF_EXPAND,
                     ((CheckBoxPreference) preference).isChecked());
             Helpers.restartSystemUI();
+            return true;
+        } else if (preference == mLongPressToKill) {
+
+            boolean checked = ((CheckBoxPreference) preference).isChecked();
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.KILL_APP_LONGPRESS_BACK, checked ? 1 : 0);
             return true;
         } else if (preference == mRecentKillAll) {
             boolean checked = ((CheckBoxPreference)preference).isChecked();
