6085bfa2681336bdc3a25b6d646f11c11126b55f
maxwen <max.weninger@gmail.com>
2013-03-01 23:33:49 +0100

    derp merge
    Change-Id: Ib8a25f79fb2895284228a7886f7b5bf418cd5b8f

--- src/com/aokp/romcontrol/fragments/UserInterface.java
+++ src/com/aokp/romcontrol/fragments/UserInterface.java
@@ -96,6 +96,7 @@ public class UserInterface extends AOKPPreferenceFragment implements OnPreferenc
     private static final CharSequence PREF_HIDE_EXTRAS = "hide_extras";
     private static final CharSequence PREF_WAKEUP_WHEN_PLUGGED_UNPLUGGED = "wakeup_when_plugged_unplugged";
     private static final CharSequence PREF_FORCE_DUAL_PANEL = "force_dualpanel";
+    private static final String NAVIGATION_BAR_STATUS_SHOW_NOW = "navigation_bar_status_show_now";
     private static final CharSequence PREF_DISABLE_BOOTANIM = "disable_bootanimation";
     private static final CharSequence PREF_CUSTOM_BOOTANIM = "custom_bootanimation";
     private static final CharSequence PREF_NOTIFICATION_VIBRATE = "notification";
@@ -235,7 +236,7 @@ public class UserInterface extends AOKPPreferenceFragment implements OnPreferenc
                         Settings.System.HIDE_EXTRAS_SYSTEM_BAR, false));
         
         mStatusBarHide = (CheckBoxPreference) findPreference(NAVIGATION_BAR_STATUS_SHOW_NOW);
-        mStatusBarHide.setChecked(Settings.System.getBoolean(cr,
+        mStatusBarHide.setChecked(Settings.System.getBoolean(mContentResolver,
                 Settings.System.NAVIGATION_BAR_STATUS_SHOW_NOW, false));
         
         mUserModeUI = (ListPreference) findPreference(PREF_USER_MODE_UI);
